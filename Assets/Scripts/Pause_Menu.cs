8f504bb1648047c3b4338e2bbd543c6fd5ef6228
Kieran-De-Sousa <kieran2.desousa@live.uwe.ac.uk>
2021-01-30 23:47:21 +0000

    Pause menu created
    Pause menu script and scene created, opened by pressing esc within the Game scene

---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool InPauseMenu = false;
+    public GameObject pauseMenuUI;
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (InPauseMenu)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    //Function called when pause menu is exited
+    public void Resume()
+    {
+        InPauseMenu = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Debug.Log("Game successfully resumed");
+    }
+    //Function called when pause menu is entered
+    void Pause()
+    {
+        InPauseMenu = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game successfully quit");
+        Application.Quit();
+    }
+}
