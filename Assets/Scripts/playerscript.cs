cb99aafc37303f501b3c83da1efe1f15be058657
JakeWarrenDavis <72136085+JakeWarrenDavis@users.noreply.github.com>
2021-03-08 01:40:55 +0000

    Jump Change
    Hold space to jump higher

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -10,12 +10,13 @@ public class playerscript : MonoBehaviour
     public bool facingRight = true;
     public float speed;
     public float move_velocity;
-    public float jump;
+    public float jump_amount;
     public Animator animator;
     private Rigidbody2D playerbody;
     public BoxCollider2D playercollider;
     public bool isAttacking = false;
     public int lifePoints;
+    private float jump_held_time;
 
     // Start is called before the first frame update
     void Start()
@@ -24,21 +25,54 @@ public class playerscript : MonoBehaviour
         playerbody = this.gameObject.GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         animator.SetBool("isAttacking", isAttacking);
         isGrounded = Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundlayer);
         animator.SetFloat("Speed", Mathf.Abs(move_velocity));
         animator.SetFloat("Jump_speed", Mathf.Abs(playerbody.velocity.y));
+
         float h = Input.GetAxis("Horizontal");
+
         if (h > 0 && !facingRight)
+        {
             Flip();
-        else if (h < 0 && facingRight)
-            Flip();
+        }
+        else
+        {
+            if (h < 0 && facingRight)
+            {
+                Flip();
+            }
+        }
+
+
+        if (!isGrounded)
+        {
+            jump_held_time = 0;
+        }
+
+
         if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && isGrounded == true)
         {
-            playerbody.velocity = new Vector2(move_velocity, jump);
+            jump_held_time += 0.01f;
+            if(jump_held_time >= 1.0f)
+            {
+                jump_held_time = 1.0f;
+                
+            }
+            Debug.Log(jump_held_time);
         }
+
+        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) && isGrounded == true)
+        {
+            playerbody.velocity = new Vector2(move_velocity, jump_amount*jump_held_time);
+            jump_held_time = 0.0f;
+        }
+
+
+
+
         move_velocity = 0;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
