6b56b47b5462176aade07998f92983c4307fe696
Kieran-De-Sousa <kieran2.desousa@live.uwe.ac.uk>
2021-03-18 13:06:39 +0000

    Death Plane

---
+++ Assets/Scripts/DeathPlaneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Plane : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject PlayerCharacter = GameObject.FindGameObjectWithTag("Player");
+        Collider2D PlayerCollider = PlayerCharacter.GetComponent<BoxCollider2D>();
+
+        if (other == PlayerCollider)
+        {
+        }
+    }
+}
